6180649d576a55c806f43b56615e96dc0d33bd70
Greta <gretafg@al.insper.edu.br>
2023-05-23 09:00:05 -0300

    tela inicial

--- tela_inicial.py
+++ tela_inicial.py
@@ -1,2 +1,42 @@
-import pygame 
+import pygame
+import random
+from os import path
+
+from config import CENARIOS_DIR, PRETO, FPS, JOGANDO, FECHAR
+
+
+def tela_inicial(tela):
+    # Variável para o ajuste de velocidade
+    relogio = pygame.time.Clock()
+
+    # Background da tela inicial
+    background = pygame.image.load(path.join(CENARIOS_DIR, 'raposa normal.png')).convert()
+    background_rect = background.get_rect()
+
+    rodando = True
+    while rodando:
+
+        # Ajusta a velocidade do jogo.
+        relogio.tick(FPS)
+
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            # Verifica se foi fechado.
+            if event.type == pygame.QUIT:
+                state = FECHAR
+                running = False
+
+            if event.type == pygame.KEYUP:
+                state = JOGANDO
+                running = False
+
+        # A cada loop, redesenha o fundo e os sprites
+        tela.fill(PRETO)
+        tela.blit(background, background_rect)
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+
+
+    return state
 
